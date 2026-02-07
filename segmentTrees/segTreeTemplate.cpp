4d55fcbc0a6ac15eb6367b05709998f71696e654
Shirisha <shirishachirra917@gmail.com>
2026-02-07 23:21:27 +0530

    Added query

--- segmentTrees/segTreeTemplate.cpp
+++ segmentTrees/segTreeTemplate.cpp
@@ -7,13 +7,19 @@ class Seg {
     
     public:
     int n; 
+    int req;
     vector <int> seg(4*n);
     Seg(int n) {
         
         this->n=n;
+        req=0;
         seg.resize(4*n,0);
     }
     
+    int combine(int ind) {
+        
+        return max([2*ind+1],seg[2*ind+2]);
+    }
     void build(int ind,int low,int high) {
         
         if(low==high) {
@@ -25,8 +31,23 @@ class Seg {
         build(2*ind+1,low,mid);
         build(2*ind+2,mid+1,high);
         
-        // perform required operation here
-        seg[ind]=seg[2*ind+1]+seg[2*ind+2];
+        combine(ind);
+    }
+    
+    int query(int ind,int low,int high,int l,int r) {
+        
+        if(high <l || low>r) {
+            return req;
+        }
+        if(low>=l && high<=r) {
+            return seg[ind];
+        }
+        int mid=(low+high)/2;
+        int left=query(2*ind+1,low,mid,l,r);
+        int right=query(2*ind+2,mid+1,high,l,r);
+        
+        return combine(left,right);
+        
     }
 };
 
